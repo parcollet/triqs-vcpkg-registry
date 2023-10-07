82ac28353bcd346129ee48d1129cb5c572bb7e36
Olivier Parcollet <oparcollet@flatironinstitute.org>
2023-10-06 22:55:22 -0400

    port

--- ports/itertools/portfile.cmake
+++ ports/itertools/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO parcollet/itertools
-        REF 95bae02
+        REF f3f8f8a
         SHA512 92b6a2322a4da284f8d6e5ad86a38ed94cf7fa817ccebe83b62fb3198d3dc0d9684bb4a38120a87a9062a889f26db6d323066dbfa5b2daaac5380b5b0c2bb63a
         HEAD_REF unstable
 )
@@ -9,6 +9,7 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
         SOURCE_PATH "${SOURCE_PATH}"
 	OPTIONS -DDO_NOT_USE_DEPS=ON
+        PREFER_NINJA
 )
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()
