ff3b6577778bbe68ac34a35e54c259e4530b2fa8
Olivier Parcollet <oparcollet@flatironinstitute.org>
2023-10-06 22:35:57 -0400

    port

--- ports/itertools/portfile.cmake
+++ ports/itertools/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
 
 vcpkg_configure_cmake(
         SOURCE_PATH "${SOURCE_PATH}"
-	OPTIONS <-DDO_NOT_USE_DEPS>
+	OPTIONS <-DDO_NOT_USE_DEPS=1>
         PREFER_NINJA
 )
 vcpkg_install_cmake()
