a4ee587270ddf5c1ec635d794bae8d3e34459543
Olivier Parcollet <oparcollet@flatironinstitute.org>
2024-11-16 20:10:25 -0500

    work on portfile

--- ports/itertools/portfile.cmake
+++ ports/itertools/portfile.cmake
@@ -6,21 +6,22 @@ set(VCPKG_POLICY_SKIP_LIB_CMAKE_MERGE_CHECK enabled)
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO parcollet/itertools
-        REF eef9fdb
+        REF 77be9d50c713a8434e6fa8ca8edcc76885dfd463
         SHA512 114ed2ef9357b6c9eb9d76f89b32f9780e4a48d6ca0f89398624e59b5429137abf5d6a5808f34af56b337ce41dfbd32acf0208d2d28bb3fb0a7d5746240b3f0e
         HEAD_REF unstable
 )
 
 vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
-	OPTIONS 
+	GENERATOR "Unix Makefiles"
+	OPTIONS	
 	  -DDO_NOT_USE_DEPS=ON
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 vcpkg_fixup_pkgconfig()
-vcpkg_copy_pdbs()
+vcpkg_copy_pdbs() # ??
 
 #file(REMOVE_RECURSE
 #    "${CURRENT_PACKAGES_DIR}/debug/include"
@@ -32,10 +33,9 @@ vcpkg_copy_pdbs()
 
 #file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(
-        INSTALL "${SOURCE_PATH}/LICENSE.txt"
-        DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-        RENAME copyright
-	)
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+     RENAME copyright
+)
 
        
