1a43d0f9716fbdc6280daee9a12aecf29f987fcb
Olivier Parcollet <oparcollet@flatironinstitute.org>
2024-11-16 20:10:51 -0500

    work on portfile

--- ports/itertools/portfile.cmake
+++ ports/itertools/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO parcollet/itertools
         REF 77be9d50c713a8434e6fa8ca8edcc76885dfd463
-        SHA512 114ed2ef9357b6c9eb9d76f89b32f9780e4a48d6ca0f89398624e59b5429137abf5d6a5808f34af56b337ce41dfbd32acf0208d2d28bb3fb0a7d5746240b3f0e
+        SHA512 88b41b46327702b90218f753e5cdaacbe3fab7c066a6d1030cf7eaa8c4419d14af555d5afb327911ea62e12828a10426a85e4c798260450212716f72bf25e318
         HEAD_REF unstable
 )
 
