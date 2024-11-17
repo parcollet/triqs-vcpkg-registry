d2c85eaa3d3fdbe5cab6e4e41fbed89d750d437c
Olivier Parcollet <oparcollet@flatironinstitute.org>
2024-11-17 16:52:31 -0500

    update port and version

--- ports/clair/portfile.cmake
+++ ports/clair/portfile.cmake
@@ -7,21 +7,21 @@ set(VCPKG_POLICY_SKIP_ABSOLUTE_PATHS_CHECK enabled)
 #include("${CMAKE_CURRENT_LIST_DIR}/../_common.cmake")
 
 # curl -L https://github.com/flatironinstitute/clair/archive/669664c14f445b9b82ab70c2dacb1bc18757c0c7.tar.gz -o tmp && vcpkg hash tmp && rm tmp
-# curl -L https://github.com/parcollet/clair/archive/669664c14f445b9b82ab70c2dacb1bc18757c0c7.tar.gz -o tmp && vcpkg hash tmp && rm tmp
+# curl -L https://github.com/parcollet/clair/archive/126361aac6efb76e45b7d1663a5af1bceb5a66e6.tar.gz -o tmp && vcpkg hash tmp && rm tmp
 # Should write a function that computes the SHA for debug purpose ? 
 # https://github.com/${REPO}/archive/${REF}.tar.gz
 
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO parcollet/clair
-        REF 669664c14f445b9b82ab70c2dacb1bc18757c0c7
-        SHA512 1d1fbb5e431e1488fd525e205859b9bd58e5ec77b3c12ed90372274684baf5cd8c41b74e2de9ecd8bac81b2b0cd1e0815d4e2fc09b77ca06bcd8660a308bde8f
+        REF 126361aac6efb76e45b7d1663a5af1bceb5a66e6
+        SHA512 62304aba3cf0c442dfbffa8f5c9f6fee811ecb8e0074ae29721f99d264d62471eccfe672d253e8fdf7de24ba71b64686c89ca13918167c17f342904d37b9a799
         HEAD_REF unstable
 )
 
 vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
-	GENERATOR Unix Makefiles
+	GENERATOR "Unix Makefiles"
 	OPTIONS	
 	-DBuild_Tests=OFF
 	-DDO_NOT_USE_DEPS=ON
