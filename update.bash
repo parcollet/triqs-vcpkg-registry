b34b2f21354cc1b5f20085b929ff4a9f6e31d5f0
Olivier Parcollet <oparcollet@flatironinstitute.org>
2024-11-17 09:43:48 -0500

    port

--- update.bash
+++ update.bash
@@ -1,5 +1,6 @@
 git ci -m "port "
 ~/vcpkg/vcpkg --x-builtin-ports-root=./ports --x-builtin-registry-versions-dir=./versions x-add-version --all --verbose --overwrite-version 
+git add verions ports
 git ci -m"version"
 git push
 git rev-parse HEAD
